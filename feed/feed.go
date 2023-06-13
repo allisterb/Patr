c624736189030afa3f7185ebbc10d9303652ba1f
Allister Beharry <allister.beharry@gmail.com>
2023-06-13 18:59:04 -0400

    Can wrap and pin legacy IPLD node.

--- feed/feed.go
+++ feed/feed.go
@@ -1,29 +1,87 @@
 package feed
 
 import (
-	"crypto/rand"
+	"context"
+	"encoding/json"
+	"os"
 
+	path "github.com/ipfs/boxo/coreiface/path"
+	cbornode "github.com/ipfs/go-ipld-cbor"
 	logging "github.com/ipfs/go-log/v2"
-	"github.com/libp2p/go-libp2p/core/crypto"
+	mh "github.com/multiformats/go-multihash"
+
+	"github.com/allisterb/patr/ipfs"
+	"github.com/allisterb/patr/node"
 )
+
+type User struct {
+	Did          string
+	NostrPrivKey []byte
+	NostrPubkey  []byte
+	IPNSPrivKey  []byte
+	IPNSPubKey   []byte
+}
+
+type Profile struct {
+	Did string
+}
 
 var log = logging.Logger("patr/feed")
 
-func GenerateIPNSKeyPair() ([]byte, []byte, error) {
-	priv, pub, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, rand.Reader)
+func init() {
+	cbornode.RegisterCborType(Profile{})
+}
+
+func LoadUser(file string) (User, error) {
+	u, err := os.ReadFile(file)
 	if err != nil {
-		log.Errorf("Error generating RSA 2048-bit keypair: %v", err)
-		return []byte{}, []byte{}, err
+		log.Errorf("could not read user file: %v", err)
+		return User{}, err
 	}
-	privkeyb, err := crypto.MarshalPrivateKey(priv)
+	var user User
+	if json.Unmarshal(u, &user) != nil {
+		log.Errorf("could not read JSON data from user file: %v", err)
+		return User{}, err
+	}
+	return user, nil
+}
+func CreateProfile(ctx context.Context, user User) error {
+	log.Infof("creating patr profile for %s...", user.Did)
+	c, err := node.LoadConfig()
 	if err != nil {
-		log.Errorf("Error marshalling RSA 2048-bit private key: %v", err)
-		return []byte{}, []byte{}, err
+		return err
 	}
-	pubkeyb, err := crypto.MarshalPublicKey(pub)
+	ipfsNode, ipfsShutdown, err := ipfs.StartIPFSNode(ctx, c.PrivKey, c.Pubkey)
 	if err != nil {
-		log.Errorf("Error marshalling RSA 2048-bit public key: %v", err)
-		return []byte{}, []byte{}, err
+		return err
 	}
-	return privkeyb, pubkeyb, err
+	profile := Profile{Did: user.Did}
+
+	n2, err := cbornode.WrapObject(profile, mh.SHA2_256, -1)
+	log.Infof("cid: %v", n2.Cid())
+	err = ipfsNode.Dag().Pinning().Add(ctx, n2)
+	bb, err := ipfsNode.Dag().Get(ctx, n2.Cid())
+	s, i, err := ipfsNode.Pin().IsPinned(ctx, path.IpldPath(bb.Cid()))
+	ipfsNode.Pin().
+		log.Infof("%v is pinned %v %v", path.IpldPath(bb.Cid()), s, i)
+	if err != nil {
+		return err
+	}
+	//ipfsNode.Dag().Pinning().
+
+	/*
+		n := bindnode.Wrap(profile, nil)
+		"github.com/ipld/go-ipld-prime/codec/dagjson"
+		cidlink "github.com/ipld/go-ipld-prime/linking/cid"
+		"github.com/ipld/go-ipld-prime/node/bindnode"
+		err = dagjson.Encode(n, &buf)
+		b, err := ipfsNode.Block().Put(ctx, &buf)
+		lnk := cidlink.Link{Cid: b.Path().Cid()}
+
+		lsys := cidlink.DefaultLinkSystem()
+		//lsys.SetReadStorage()
+	*/
+
+	ipfsShutdown()
+	return err
 }
